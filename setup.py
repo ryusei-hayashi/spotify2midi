d025185eede513ae9f3da50593612afb412c6c0a
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-05-11 16:17:47 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'spotify2midi',
-    version = '0.1',
+    version = 'beta',
     description = "This package creates MIDI file from Spotify URL using Get Track's Audio Analysis of Spotify Web API.",
     install_requires = ['spotipy', 'numpy', 'mido'],
     packages = find_packages()
