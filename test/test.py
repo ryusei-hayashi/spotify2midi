94988129863e12e21d71259b0feb6729601e8a88
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-10-03 14:17:21 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 import spotify2midi
 
-id = ## YOUR SPOTIFY ID ##
-secret = ## YOUR SPOTIFY SECRET ##
+id = '''YOUR SPOTIFY ID'''
+secret = '''YOUR SPOTIFY SECRET'''
 s2m = spotify2midi.Spotify2Midi(id, secret)
 
 url = 'https://open.spotify.com/track/5Yx45WDFNYLFwj3pjtvfJ6'
